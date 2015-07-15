6d9cca0631e24f55082b8b78cef0388d20ecff83
hsbc <hsbc@hsbcdeMac-mini.local>
2015-07-15 09:21:00 +0800

    Initial Commit

---
+++ 仿TodayHeader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  仿TodayHeader
+//
+//  Created by hsbc on 15/7/15.
+//  Copyright (c) 2015年 Zp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
